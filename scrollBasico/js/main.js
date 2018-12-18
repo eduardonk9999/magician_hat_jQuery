0ed79d5a5ae9279c02e329361d5390833e24cd00
eduardo <eduardo@okn.com.br>
2018-12-18 09:59:49 -0200

    Primeiro Truque

---
+++ scrollBasico/js/main.js
@@ -0,0 +1,7 @@
+// Dica: Quebre em pequenas partes, procure códigos semelhantes e teste.
+
+// 1 - Distância entre o scroll e o topo
+// 2 - Ditância entre o elemento e o topo
+// 3 - Verificar essas duas variáveis sempre que o scroll for ativado
+// 4 - Se distância entre o elemento e o topo for maior que do scroll, adicionar classe
+// 5 - A classe deve mostrar e animar o elemento
